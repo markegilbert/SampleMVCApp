0700e0a9e9bfcda72c68eccbca2787b63a990e2f
Mark Gilbert <5085540+markegilbert@users.noreply.github.com>
2024-10-09 16:15:42 -0400

    Added initial unit test project

---
+++ UnitTests/SampleAppConfigTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using SampleApp.Config;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class SampleAppConfigTests
+    {
+
+        [Test]
+        public void InstantiateClass_ConnectionStringsIsInitialized()
+        {
+            SampleAppConfig config = new SampleAppConfig();
+            Assert.That(config.ConnectionStrings, Is.Not.Null);
+        }
+
+
+    }
+}
