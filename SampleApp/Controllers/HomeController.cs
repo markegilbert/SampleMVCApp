acfc5fe85dad1bb9a5b505de751ed3e329de8ab3
Mark Gilbert <5085540+markegilbert@users.noreply.github.com>
2024-10-11 16:36:00 -0400

    Cleaned up HomeController and views a bit

--- SampleApp/Controllers/HomeController.cs
+++ SampleApp/Controllers/HomeController.cs
@@ -9,27 +9,6 @@ namespace SampleApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _Logger;
-        private readonly ChinookDbContext _Context;
-
-        public HomeController(ILogger<HomeController> Logger, IOptions<GeniusAPISettings> Config, ChinookDbContext Context)
-        {
-            // TODO: Validate these
-            _Logger = Logger;
-            //_Config = Config;
-            _Context = Context;
-
-            // TODO: Remove these when done testing
-            var Artists = _Context.GetAllArtists();
-            _Logger.LogInformation($"Total number of artists found: {Artists.Count}");
-
-            var Albums = _Context.GetAllAlbums();
-            _Logger.LogInformation($"Total number of albums found: {Albums.Count}");
-
-            var Tracks = _Context.GetAllTracks();
-            _Logger.LogInformation($"Total number of tracks found: {Tracks.Count}");
-        }
-
         public IActionResult Index()
         {
             return View();
