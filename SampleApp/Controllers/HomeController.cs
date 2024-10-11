4d537e3770fd28897f594cc98f14922131ef20d9
Mark Gilbert <5085540+markegilbert@users.noreply.github.com>
2024-10-11 15:57:15 -0400

    Removing unneeded views; cleaning up the remaining ones

--- SampleApp/Controllers/HomeController.cs
+++ SampleApp/Controllers/HomeController.cs
@@ -35,15 +35,11 @@ namespace SampleApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
